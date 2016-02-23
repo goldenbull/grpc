410ea6a88fb395fd963da79e3e8ca6f7af017b19
goldenbull <goldenbull@gmail.com>
2016-02-23 10:39:01 +0800

    add a build script

---
+++ examples/cpp/build_all_vs2015.bat
@@ -0,0 +1,64 @@
+@set nuget=%~dp0..\..\..\hush\nuget\nuget.exe
+@pushd ..\..\
+@call "%VS140COMNTOOLS%\vsvars32.bat"
+
+@rem --------------------------------------------------------------------------------------
+@rem build protobuf to generate protoc.exe
+pushd third_party\protobuf\cmake
+cmake -G "Visual Studio 14 2015" -Dprotobuf_BUILD_TESTS=OFF . || goto :error
+@rem for x64: cmake -G "Visual Studio 14 2015" -A "x64" -Dprotobuf_BUILD_TESTS=OFF . || goto :error
+msbuild protobuf.sln /p:Configuration=Release /m || goto :error
+popd
+
+@rem --------------------------------------------------------------------------------------
+@rem build C# protobuf lib
+pushd third_party\protobuf\csharp\src
+%nuget% restore Google.Protobuf.sln || goto :error
+msbuild Google.Protobuf.sln /p:Configuration=Debug /m || goto :error
+msbuild Google.Protobuf.sln /p:Configuration=Release /m || goto :error
+popd
+
+@rem --------------------------------------------------------------------------------------
+@rem build grpc plugins for protoc.exe
+pushd vsprojects
+%nuget% restore grpc_protoc_plugins.sln || goto :error
+msbuild grpc_protoc_plugins.sln /p:Configuration=Release /m || goto :error
+popd
+
+@rem --------------------------------------------------------------------------------------
+@rem build grpc C# extension
+pushd vsprojects
+%nuget% restore grpc_csharp_ext.sln || goto :error
+msbuild grpc_csharp_ext.sln /p:Configuration=Debug;Platform=Win32 /m || goto :error
+msbuild grpc_csharp_ext.sln /p:Configuration=Release;Platform=Win32 /m || goto :error
+msbuild grpc_csharp_ext.sln /p:Configuration=Debug;Platform=x64 /m || goto :error
+msbuild grpc_csharp_ext.sln /p:Configuration=Release;Platform=x64 /m || goto :error
+popd
+
+@rem --------------------------------------------------------------------------------------
+@rem build grpc C# lib
+pushd src\csharp
+%nuget% restore Grpc.sln || goto :error
+msbuild Grpc.sln /p:Configuration=Debug /m || goto :error
+msbuild Grpc.sln /p:Configuration=Release /m || goto :error
+popd
+
+@rem --------------------------------------------------------------------------------------
+@rem build grpc lib
+pushd vsprojects
+%nuget% restore grpc-test.sln || goto :error
+msbuild grpc-test.sln /p:Configuration=Debug;Platform=Win32 /m || goto :error
+msbuild grpc-test.sln /p:Configuration=Release;Platform=Win32 /m || goto :error
+msbuild grpc-test.sln /p:Configuration=Debug;Platform=x64 /m || goto :error
+msbuild grpc-test.sln /p:Configuration=Release;Platform=x64 /m || goto :error
+popd
+
+@echo *************** Success! ***************
+@goto :end
+
+:error
+@echo *************** Failed! ***************
+
+:end
+popd
+pause
